8f0ec8211af9b44da0d406b1ba71629bfa7c4eda
Geisson Iuri <geissoniuri@gmail.com>
2021-09-27 05:39:00 -0300

    aula 02

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # rocketseat nlw together nodejs
  curso rocketseat node js
+
+
+## NLW Valoriza
+
+## Regras
+
+- Cadastro de usuario
+  [ ] Não é permitido cadastrar mais de um usuário com o mesmo e-mail
+  [ ] Não é permitido cadastrar usuario sem e-mail
+
+- Cadastro de TAG
+  [ ] Não é permitido cadastrar mais de uma tag com o mesmo nome
+  [ ] Não é permitido cadastrar tag sem nome
+  [ ] Não é permitido o cadastro por usuarios que não sejam administradores
+
+- Cadastro de elogios
+  [ ] Não é permitido um usuario cadastrar um elogio para si
+  [ ] Não é permitido cadastrar elogios para usuarios invalidos
+  [ ] O Usuario precisa estar autenticado na aplicação
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,20 @@
+import 'reflect-metadata';
 import express from "express"
+import { router } from './routes';
+import './database'
 
 const app = express()
 
-/**
- * GET    => buscar uma informação
- * POST   => inserir ou criar uma informação
- * PUT    => alterar uma informação
- * DELETE => remover um dado
- * PATCH  => alterar uma informação especifica
- */
+app.use(express.json())
+
+app.use(router)
 
 app.get('/test', (request, response) => {
-  // request  => entrando
-  // response => saindo
-  return response.send('ola nlw')
+  return response.send('Ola nlw')
 })
 
 app.post('/test-post', (request, response) => {
-  return response.send('ola, nwl metodo post')
+  return response.send('nlw metodo post')
 })
 
 app.listen(3000, () => console.log('server is running nlw'))
